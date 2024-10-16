2a61f1cfedd849884e4b4b06f895356e99b8db36
12618 <1261801883@qq.com>
2024-10-16 21:08:31 +0800

    2024-10-16 1、完成模拟调用服务代理对象生成的逻辑

---
+++ dd-rpc-core/src/main/java/com/dd/ddrpc/proxy/ServiceProxyFactory.java
@@ -0,0 +1,43 @@
+package com.dd.ddrpc.proxy;
+
+import com.dd.ddrpc.RpcApplication;
+import com.dd.ddrpc.config.RpcConfig;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * 服务代理工厂 （工厂模式 用于创建代理对象）
+ */
+public class ServiceProxyFactory {
+
+    /**
+     * 根据服务类获取代理对象
+     * @param serviceClass
+     * @return
+     * @param <T>
+     */
+    public static <T> T getProxy(Class<T> serviceClass) {
+        //根据配置文件获取配置类 并判断是否是Mock对象
+        if (RpcApplication.getRpcConfig().isMock()) {
+            return getMockProxy(serviceClass);
+        }
+
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new ServiceProxy());
+    }
+
+    /**
+     * 根据服务类获取 Mock 代理对象
+     * @param serviceClass
+     * @return
+     * @param <T>
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass) {
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new MockServiceProxy());
+    }
+}
